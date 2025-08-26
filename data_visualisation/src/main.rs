7d5c1f5b4b9a2e23dc66fe9d8c5aaef925f73abf
Elliot Sand-Warge <elliotsandmail@gmail.com>
2025-08-26 11:41:38 +0200

    developed visualisation of the coordinates

---
+++ data_visualisation/src/main.rs
@@ -0,0 +1,73 @@
+use std::{env, vec};
+
+use dotenv::dotenv;
+use plotly::common::{Marker, Mode};
+use plotly::layout::{Axis, Layout};
+use plotly::{Plot, Scatter};
+use postgres::{Client, Error, NoTls};
+
+struct CoordinatesObject {
+    latitudes: Vec<f64>,
+    longitudes: Vec<f64>,
+}
+
+impl CoordinatesObject {
+    fn new() -> Self {
+        CoordinatesObject {
+            latitudes: Vec::new(),
+            longitudes: Vec::new()
+        }
+    }
+}
+
+fn get_coordinates() -> Result<CoordinatesObject, Error> {
+    dotenv().ok();
+
+    let postgres_url: String =
+        env::var("POSTGRES_URL").expect("POSTGRES_URL must be set");
+
+    let mut client: Client = Client::connect(
+        &postgres_url,
+        NoTls,
+    )?;
+
+    let mut coordinates_object: CoordinatesObject = CoordinatesObject::new();
+
+    for row in client.query("SELECT latitude, longitude FROM villa", &[])? {
+        coordinates_object.latitudes.push(row.get(0));
+        coordinates_object.longitudes.push(row.get(1));
+    }
+
+    Ok(coordinates_object)
+}
+
+fn visualise_locations() -> () {
+    let coordinates_object: CoordinatesObject = match get_coordinates() {
+        Ok(result) => result,
+        Err(e) => {
+            eprintln!("Failed to get coordinates: {}", e);
+            return;
+        }
+    };
+
+    let trace = Scatter::new(coordinates_object.longitudes, coordinates_object.latitudes)
+        .mode(Mode::Markers)
+        .marker(Marker::new().size(2));
+
+    let layout = Layout::new()
+        .width(400)
+        .height(700)
+        .title("Coordinates for all houses in the training set")
+        .x_axis(Axis::new().title("Longitude").range(vec![11.0270, 24.1450]).scale_anchor("x"))
+        .y_axis(Axis::new().title("Latitude").range(vec![55.3419, 69.0597]));
+
+    let mut plot = Plot::new();
+    plot.add_trace(trace);
+    plot.set_layout(layout);
+
+    plot.show();
+}
+
+fn main() {
+    visualise_locations();
+}
